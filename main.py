0e0c87271625138bae9d029cce6ca345cd96eb04
jeremy matt <jeremy.matt@gmail.com>
2025-04-05 21:20:15 -0400

    update code

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ import serial
 # app = Flask(__name__)
 # app.secret_key = 'your_secret_key_here'  # Replace with a strong secret key
 
-
 # Set up the serial port for UART communication
 # brookside_uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=0.5)
 # roadside_uart = serial.Serial("/dev/ttyAMA5", baudrate=9600, timeout=0.5)
