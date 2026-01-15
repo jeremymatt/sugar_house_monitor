1c3adb1ffb5f4e14b4a28ec4ce26ff52abca1121
jeremymatt <jeremy.matt@gmail.com>
2026-01-14 23:29:57 -0500

    try to update environment.sh

--- scripts/display_pi_setup/setup_environment.sh
+++ scripts/display_pi_setup/setup_environment.sh
@@ -7,4 +7,5 @@ REQ_FILE="${SCRIPT_DIR}/requirements.txt"
 
 python3 -m venv --system-site-packages "${VENV_PATH}"
 "${VENV_PATH}/bin/pip" install --upgrade pip
+"${VENV_PATH}/bin/pip" install -r wheel
 "${VENV_PATH}/bin/pip" install -r "${REQ_FILE}"
